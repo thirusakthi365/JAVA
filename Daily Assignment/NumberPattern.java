1218b2a52796842983d25e933720b3a3e381e088
thirusakthi365 <99789580+thirusakthi365@users.noreply.github.com>
2022-05-29 11:54:32 +0530

    Create NumberPattern.java

---
+++ Daily Assignment/NumberPattern.java
@@ -0,0 +1,15 @@
+class NumberPattern
+{
+public static void main (String [] args)
+{
+int i,j,row = 5; int number = 1;
+for ( i = 1; i< = row; i++)
+{
+for(j=1; j<i+1; j++)
+{
+System.out.println(number+++ "  ");
+}
+System.out.println();
+}
+}
+}
