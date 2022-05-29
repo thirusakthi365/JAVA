c91d3f8628e580f27c2c7d9a6de0b15821e8837e
thirusakthi365 <99789580+thirusakthi365@users.noreply.github.com>
2022-05-29 11:58:13 +0530

    Create Sum.java

---
+++ Daily Assignment/Sum.java
@@ -0,0 +1,12 @@
+public class Sum{
+public void add(int a, int b)
+{
+int c = a+b;
+System.out.println("Sum is :  " +c);
+}
+public static void main (String [] args )
+{
+Test obj = new Test();
+obj.add(2,10);
+}
+}
