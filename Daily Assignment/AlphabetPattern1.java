4aeb73b298722aacb61085f583fdb2d24ea6d8f7
thirusakthi365 <99789580+thirusakthi365@users.noreply.github.com>
2022-05-29 11:47:32 +0530

    Create AlphabetPattern1.java

---
+++ Daily Assignment/AlphabetPattern1.java
@@ -0,0 +1,16 @@
+class AlphabetPattern1
+{
+public static void main (String [] args)
+{
+int i,j,row = 5 ;
+int alphabet = 65; 
+for ( i = 0; i<= row; i++)
+{
+for(j=0; j<=i; j++)
+{
+System.out.print((char) (alphabet++)+"   ");
+}
+System.out.println();
+}
+}
+}
